f1789fe1c9babaa25cdbc5e25e74381403741dc7
viji738 <balabala400044@gmail.com>
2026-08-20 14:22:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -237,7 +237,7 @@ if (loginForm) {
 
                 const response =
                     await fetch(
-                        "http://127.0.0.1:5000/login",
+                        "https://ai-roster-backend.onrender.com/login",
                         {
                             method: "POST",
 
